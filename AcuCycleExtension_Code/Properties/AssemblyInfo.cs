40d0470ce58e5f43dcd73b969e34fdf755b3dc08
Leonardo Justiniano <ljustiniano@theanswerco.com>
2024-01-27 14:26:41 -0800

    Draft1 Extensions

---
+++ AcuCycleExtension_Code/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Acumatica Customization")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hackathon")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright 2024. All rights reserved")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c34c040b-825e-40b2-9d6c-da6522176bcd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("22.212.0046.2305")]
+[assembly: AssemblyFileVersion("22.212.0046.2305")]
+[assembly: NeutralResourcesLanguage("en-US")]
