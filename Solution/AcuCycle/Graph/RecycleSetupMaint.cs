bb48e3b7d1384cc733da9e113ff7c88b79e0469d
Evan Templin <etemplin@crestwood.com>
2024-01-27 13:57:24 -0800

    added preferences

---
+++ Solution/AcuCycle/Graph/RecycleSetupMaint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PX.Data;
+
+namespace AcuCycle
+{
+    public class RecycleSetupMaint : PXGraph<RecycleSetupMaint>
+    {
+        #region Views
+        public PXSave<RecycleSetup> Save;
+        public PXCancel<RecycleSetup> Cancel;
+
+        public PXFilter<RecycleSetup> Setup;
+        #endregion
+    }
+}
